65f1d45723851343ab34c4910f863b7461220d94
Samy SIDHOUM <samysidhoum6@gmail.com>
2024-10-24 16:05:26 +0200

    Ajout d'un bouton déclenchant une action

--- src/app/face-snap/face-snap.component.ts
+++ src/app/face-snap/face-snap.component.ts
@@ -13,6 +13,8 @@ export class FaceSnapComponent {
   createdAt!: Date;
   snaps!: number;
   imageUrl!: string;
+  snapButtonText!: string;
+  userHasSnapped!: boolean;
 
   ngOnInit(): void {
     this.title = 'Test';
@@ -20,5 +22,28 @@ export class FaceSnapComponent {
     this.createdAt = new Date();
     this.snaps = 3;
     this.imageUrl = 'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg';
+    this.snapButtonText = 'Oh Snap!';
+    this.userHasSnapped = false;
   }
+
+  onSnap() : void {
+    if (this.userHasSnapped) {
+      this.unSnap();
+    }else{
+      this.snap();
+
+    }
+}
+
+unSnap() {
+  this.snaps--;
+  this.snapButtonText = 'Oh Snap!';
+  this.userHasSnapped = false;
+}
+
+snap() {
+  this.snaps++;
+  this.snapButtonText = 'Oops, unSnap!';
+  this.userHasSnapped = true;
+}
 }
