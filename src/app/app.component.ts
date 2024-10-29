00b3a7735f05d51fe1b779db0cffc74a79859c87
Samy SIDHOUM <samysidhoum6@gmail.com>
2024-10-29 11:26:10 +0100

    Ajout de propriétés personnalisées

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,34 +12,33 @@ import { FaceSnap } from './models/face-snap';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit{
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
+ 
+  faceSnaps!: FaceSnap[];
 
   ngOnInit(): void {
-    this.mySnap = new FaceSnap(
-      'Test',
-      'Lorem lapsus',
-      'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
-      new Date(),
-      3
-    );
-    this.myOtherSnap = new FaceSnap(
-      'Test 1',
-      'Lorem lapsus',
-      'https://cdn.pixabay.com/photo/2018/08/31/18/21/fantasy-3645269_1280.jpg',
-      new Date(),
-      7
-    );
-
-    this.myLastSnap = new FaceSnap(
-      'Test 2',
-      'Lorem lapsus',
-      'https://cdn.pixabay.com/photo/2023/10/24/02/49/bike-8337261_1280.jpg',
-      new Date(),
-      9
-    );
-
-    this.myOtherSnap.setLocation('toto');
+    this.faceSnaps = [
+      new FaceSnap(
+        'Test',
+        'Lorem lapsus',
+        'https://cdn.pixabay.com/photo/2015/05/31/16/03/teddy-bear-792273_1280.jpg',
+        new Date(),
+        3
+      ),
+      new FaceSnap(
+        'Test 1',
+        'Lorem lapsus',
+        'https://cdn.pixabay.com/photo/2018/08/31/18/21/fantasy-3645269_1280.jpg',
+        new Date(),
+        7
+      ),
+      new FaceSnap(
+        'Test 2',
+        'Lorem lapsus',
+        'https://cdn.pixabay.com/photo/2023/10/24/02/49/bike-8337261_1280.jpg',
+        new Date(),
+        9
+      )
+    ];
+    this.faceSnaps[1].setLocation('toto');
   }
 }
